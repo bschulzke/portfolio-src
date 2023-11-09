afaf8b9ed73507442b5f9cb431286d4ea8dfa48d
Ben Schulzke <benschulzke@gmail.com>
2023-11-09 00:55:03 -0700

    set up email-js form submission

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,29 +3,29 @@
     <!-- Wrapper container -->
     <div class="container py-4">
         <!-- Bootstrap 5 starter form -->
-        <form id="contactForm">
+        <form ref="form" id="contactForm" @submit.prevent="sendEmail">
 
             <!-- Name input -->
             <div class="mb-3">
             <label class="form-label" for="name">Name</label>
-            <input class="form-control" id="name" type="text" placeholder="Name" />
+            <input class="form-control" id="name" type="text" placeholder="Name" name="user_name"/>
             </div>
 
             <!-- Email address input -->
             <div class="mb-3">
             <label class="form-label" for="emailAddress">Email Address</label>
-            <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" />
+            <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" name="user_email"/>
             </div>
 
             <!-- Message input -->
             <div class="mb-3">
             <label class="form-label" for="message">Message</label>
-            <textarea class="form-control" id="message" type="text" placeholder="Message" style="height: 10rem;"></textarea>
+            <textarea name="message" class="form-control" id="message" type="text" placeholder="Message" style="height: 10rem;"></textarea>
             </div>
 
             <!-- Form submit button -->
             <div class="d-grid">
-            <button class="btn btn-primary btn-lg" type="submit">Submit</button>
+            <button class="btn btn-primary btn-lg" type="submit">Send</button>
             </div>
 
         </form>
@@ -39,6 +39,31 @@
         </div>
   </div>
 </template>
+
+<script>
+import emailjs from 'emailjs-com';
+export default {
+      name: 'ContactMe',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+ methods: {
+    sendEmail() {
+      emailjs.sendForm('service_8ttm8yk', 'template_3sti73b', this.$refs.form, '99wZUEga7zsJwe-6X')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
+    }
+  }
+}
+
+</script>
 
 <style scoped>
 .contact {
